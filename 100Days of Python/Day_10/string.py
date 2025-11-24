ff53182a34d876c15eca978368ec016025d8d104
MD AMIT HASAN ROBI <amithasanrobi10@gmail.com>
2025-11-24 23:53:31 +0530

    Day 11

--- 100Days of Python/Day_10/string.py
+++ 100Days of Python/Day_10/string.py
@@ -1,9 +1,14 @@
-# name = "robi"
-# friend = "sajib"
-# anotherfriend = "rahim"
-# print( "Hello" , name )
-# print("Hello", " " , friend)
-# print("Hello" + " " + anotherfriend)
+name = "robi"
+friend = "sajib"
+anotherfriend = "rahim"
+print( "Hello" , name )
+print("Hello", " " , friend)
+print("Hello" + " " + anotherfriend)
 
-a = "He said, " '\He wants to eat apple'
-print(a)
+name = "AMIT"
+print(name[0]) #We can access the characters of a string using indexing
+print(name[2])
+
+z = "ROBINDRO"
+for character in z: #You can use a for loop to go through each character in a string.
+    print(character)
