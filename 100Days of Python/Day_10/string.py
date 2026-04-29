5b65eb06f178ae05a9c95886e55af0e06d73b8e5
MD AMIT HASAN ROBI <amithasanrobi10@gmail.com>
2026-04-29 21:44:24 +0530

    Update string.py

--- 100Days of Python/Day_10/string.py
+++ 100Days of Python/Day_10/string.py
@@ -8,7 +8,7 @@ print("Hello" + " " + anotherfriend)
 name = "AMIT"
 print(name[0]) #We can access the characters of a string using indexing
 print(name[2])
-
-z = "ROBINDRO"
-for character in z: #You can use a for loop to go through each character in a string.
-    print(character)
+print("lets try to change the name\n")
+z = "ROBINDRONATH"
+for lie in z: #You can use a for loop to go through each character in a string.
+    print(lie)
